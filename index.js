7199f5856958ea910575edd45bb4d98003787827
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-11-11 04:10:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function printBadges(anArray){
+  for (let i = 0; i < anArray.length; i++) {
+    console.log(`Welcome ${anArray[i]}! You are employee #${i+1}.`)
+  }
+return anArray;
+}
+function tailsNeverFails(){
+  let counter = 0;
+  let coin = Math.random();
+  while (coin > 0.5) {
+    counter++
+  }
+return `You got ${counter} tails in a row!`
+}
